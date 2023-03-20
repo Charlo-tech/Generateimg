31c54466cde0396ac62a0301a2c0fbb816af637f
Ruita <charliewachira124@gmail.com>
2023-03-20 14:11:47 +0300

    added flag

--- artt.py
+++ artt.py
@@ -20,7 +20,25 @@ def generate_artt():
         line_xy = (i, 0, i, 480)
         line_color = (255, 255, 255)
         draw.line(line_xy, fill=line_color, width=6)
-    
+
+    #draw flag base
+    flag_bs = (60, 60, 420, 340)
+    flag_color = (255, 255, 255)
+    draw.rectangle(flag_bs, fill=flag_color)
+
+    flag_bl = (60, 60, 420, 140)
+    flag_color = (0, 0, 0)
+    draw.rectangle(flag_bl, fill=flag_color)
+
+    flag_rd = (60, 160, 420, 240)
+    flag_color = (255, 0, 0)
+    draw.rectangle(flag_rd, fill=flag_color)
+
+    flag_gn = (60, 260, 420, 340)
+    flag_color = (0, 128, 0)
+    draw.rectangle(flag_gn, fill=flag_color)
+
+    #draw letter A
     line_bn = (240, 0, 480, 480)
     line_color = (0, 0, 255)
     draw.line(line_bn, fill=line_color, width=15)
@@ -33,6 +51,13 @@ def generate_artt():
     line_color = (0, 0, 255)
     draw.line(line_bn, fill=line_color, width=15)
 
+    #add background to text
+    bg_bn = (60, 360, 420, 420)
+    bg_color = (0, 0, 0)
+    draw.rectangle(bg_bn, fill=bg_color)
+
+
+    #add text to image
     myfont = ImageFont.truetype("arial.ttf", 60)
     draw.text((60, 360), "ANGELHACK", font=myfont, fill=(255, 255, 0))
     
